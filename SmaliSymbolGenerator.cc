c15e45b6d2b61ab94dcc1f48631789e7e69ef95a
JeffSeto <jeffreyseto1@gmail.com>
2017-08-14 01:23:07 -0400

    Started work on Smali token generator

--- SmaliSymbolGenerator.cc
+++ SmaliSymbolGenerator.cc
@@ -1,6 +1,8 @@
 #include "SmaliSymbolGenerator"
 
-Token* SmaliSymbolGenerator::generateToken(std::string l){
+
+
+Token* SmaliTokenGenerator::generateToken(std::string l){
 	Token *token = new Token;
 	if(l == "("){
 		token->type = "LPAREN";
@@ -12,65 +14,93 @@ Token* SmaliSymbolGenerator::generateToken(std::string l){
 		token->type = "RBRACE";
 	} else if(l == "return"){
 		token->type = "RETURN";
-	} else if(l == "if"){
-		token->type = "IF";
-	} else if(l == "else"){
-		token->type = "ELSE";
-	} else if(l == "while"){
-		token->type = "WHILE";
-	} else if(l == "println"){
-		token->type = "PRINTLN";
-	} else if(l == "wain"){
-		token->type = "WAIN";
-	} else if(l == "="){
-		token->type = "BECOMES";
-	} else if(l == "int"){
-		token->type = "INT";
-	} else if(l == "=="){
-		token->type = "EQ";
-	} else if(l == "!="){
-		token->type = "NE";
-	} else if(l == "<"){
-		token->type = "LT";
-	} else if(l == ">"){
-		token->type = "GT";
-	} else if(l == "<="){
-		token->type = "LE";
-	} else if(l == ">="){
-		token->type = "GE";
-	} else if(l == "+"){
-		token->type = "PLUS";
-	} else if(l == "-"){
-		token->type = "MINUS";
-	} else if(l == "*"){
-		token->type = "STAR";
-	} else if(l == "/"){
-		token->type = "SLASH";
-	} else if(l == "%"){
-		token->type = "PCT";
-	} else if(l == ","){
-		token->type = "COMMA";
-	} else if(l == ";"){
-		token->type = "SEMI";
-	} else if(l == "new"){
-		token->type = "NEW";
-	} else if(l == "delete"){
-		token->type = "DELETE";
-	} else if(l == "["){
-		token->type = "LBRACK";
-	} else if(l == "]"){
-		token->type = "RBRACK";
-	} else if(l == "&"){
-		token->type = "AMP";
-	} else if(l == "NULL"){
-		token->type = "NULL";
 	} else {
-		token->type = "ERR";
+		std::string type = dotKeyword(l);
+		if(type != "ERROR") {
+			token->type = type;
+			token->lexeme = l;
+			return token;
+		}
+
+		type = generalKeyword(std::string l);
+		if(type != "ERROR") {
+			token->type = type;
+			token->lexeme = l;
+			return token;
+		}
 	}
+
 	token->lexeme = l;
 	return token;
 }
 
+std::string SmaliSymbolGenerator::generalKeyword(std::string l) {
+	if(l == ".array-data"){
+		return "ARRAY-DATA";
+	} else if(l == ".catch"){
+		return "CATCH";
+	} else if(l == ".catchall"){
+		return "CATCHALL";
+	} else if(l == ".class"){
+		return "CLASS";
+	} else if(l == ".end"){
+		return "END";
+	} else if(l == ".enum"){
+		return "ENUM";
+	} else if(l == ".epilogue"){
+		return "EPILOGUE";
+	} else if(l == ".field"){
+		return "FIELD";
+	} else if(l == ".implements"){
+		return "IMPLEMENTS";
+	} else if(l == ".line"){
+		return "LINE";
+	} else if(l == ".local"){
+		return "LOCAL";
+	} else if(l == ".locals"){
+		return "LOCALS";
+	} else if(l == ".parameter"){
+		return "PARAMETER";
+	} else if(l == ".proglogue"){
+		return "PROLOGUE";
+	} else if(l == ".registers"){
+		return "REGISTERS";
+	} else if(l == ".restart"){
+		return "RESTART";
+	} else if(l == ".source"){
+		return "SOURCE";
+	} else if(l == ".subannotation"){
+		return "SUBANNOTATION";
+	} else {
+		return "ERROR";
+	}
+}
+
+std::string SmaliSymbolGenerator::generalKeyword(std::string l) {
+
+}
+
+//Return generator instance
 SmaliSymbolGenerator SmaliSymbolGenerator::getInstance() {
 	return instance;
+}
+
+//Private state function
+void stateFunction(DFAState* state, char v) {
+	
+}
+
+void SmaliSymbolGenerator::convert(std::istream& stream, const SmaliSymbolGenerator& generator) {
+	std::string line;
+	getline(stream, line);
+	DFAState* state = NULL;
+	while(line != NULL) {
+		for(int i = 0; i < line.length(); i++) {
+			char c = line[i];
+			if(!state) {
+				state = new State();
+			}
+			
+		}
+	}
 }
--- SmaliSymbolGenerator.h
+++ SmaliSymbolGenerator.h
@@ -1,23 +1,41 @@
 #ifndef _SMALI_SYMBOL_GENERATOR_
 #define _SMALI_SYMBOL_GENERATOR_
 
-
 #include <vector>
 #include <string>
+#include "SmaliTokenGenerator.h"
 
 class SmaliSymbolGenerator {
 	private:
 		static SmaliSymbolGenerator instance;
 		SmaliSymbolGenerator();
 		
+		Token* generateToken(std::string);
+
+		std::string dotKeyword(std::string) const;
+		void stateFunction(State* state, char v);
 		
+		enum State {empty}
+
+		struct DFAState{
+			int state;
+			std::string lexeme;
+			State() {
+				state = 0;
+				lexeme = "";
+			}
+		}
 
 	public:
 		struct Token{
 			std::string lexeme;
 			std::string type;
 		};
+
 		
+		//Input operator
+		void convert(std::istream& stream);
+
 		//Returns Symbol Generator instance
 		SmaliSymbolGenerator getInstance();
 
